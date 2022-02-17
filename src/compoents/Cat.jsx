4f0f402a8a03217cd93e5a887c46c31781cde9c5
Prs2630 <poojakinholkar1998@gmail.com>
2022-02-17 20:08:46 +0530

    added some changes

--- src/compoents/Cat.jsx
+++ src/compoents/Cat.jsx
@@ -41,6 +41,27 @@ const Cat = [
         price: 4500,
         category: "All",
         image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439702/item/ingoods_74_439702.jpg?width=1600&impolicy=quality_75"
+    },
+    {
+        id: 7,
+        title: "Mens Jacket",
+        price: 3500,
+        category: "Black",
+        image: "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010803772-Black-1000010803772_01-2100.jpg"
+    },
+    {
+        id: 8,
+        title: "Watch",
+        price: 2500,
+        category: "All",
+        image: "https://www.reliancedigital.in/medias/Noise-Colorfit-Pro-2-Smart-Watch-491900986-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w1MDc0NXxpbWFnZS9qcGVnfGltYWdlcy9oYzkvaDdjLzk1OTE4MjA3NDY3ODIuanBnfDA3MjZlMzkzOTExYzMyNzk5Y2YzOTk0MWFkMjc5YjE5OTlkMDE4NzYzYTg1YzQ2YmNjZjMxYzc5ZDJiYjBlNDg"
+    },
+    {
+        id: 9,
+        title: "Laptop",
+        price: 69000,
+        category: "All",
+        image: "https://www.lenovo.com/medias/lenovo-laptop-ideapad-3-15-intel-hero.png?context=bWFzdGVyfHJvb3R8MzAzNDQ1fGltYWdlL3BuZ3xoNjYvaDY2LzEwNzU3MjQxNTAzNzc0LnBuZ3xjMzU3NWY4OGEyYjYzYTEwOGFlYzhiNWJhODEwYzA1MTlkNDYxODI3ZGQxM2IzYTRhYmY4M2YzY2NjYjhhOGJj"
     }
 
 ]
--- src/compoents/Category.jsx
+++ src/compoents/Category.jsx
@@ -3,9 +3,9 @@ import Cat from './Cat';
 
 const Category = () => {
   const [data, setData] = useState(Cat);
-  const filterresult=(catItem)=>{
-    const result=Cat.filter((curData)=>{
-      return curData.category===catItem
+  const filterresult = (catItem) => {
+    const result = Cat.filter((curData) => {
+      return curData.category === catItem
 
     })
     setData(result)
@@ -17,12 +17,12 @@ const Category = () => {
       <div className='container-fluid mx-2'>
         <div className='row mt-5 mx-2 ' >
           <div className='col-md-3'>
-            <button className='btn btn-warning w-100 mb-4'onClick={()=>filterresult('Men')}>Men</button>
-            <button className='btn btn-warning w-100 mb-4'onClick={()=>filterresult('Women')} >Women</button>
-            <button className='btn btn-warning w-100 mb-4'onClick={()=>filterresult('Children')} >Children</button>
-            <button className='btn btn-warning w-100 mb-4'onClick={()=>filterresult('Black')} >Black</button>
-            <button className='btn btn-warning w-100 mb-4' onClick={()=>filterresult('White')}>White</button>
-            <button className='btn btn-warning w-100 mb-4'onClick={()=>filterresult('All')} >All</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('Men')}>Men</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('Women')} >Women</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('Children')} >Children</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('Black')} >Black</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('White')}>White</button>
+            <button className='btn btn-warning w-100 mb-4' onClick={() => filterresult('All')} >All</button>
 
           </div>
           <div className='col-md-9'>
@@ -36,12 +36,15 @@ const Category = () => {
                         <div className="card" >
                           <img src={image} className="card-img-top" alt="..." />
                           <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p>
+                            <h5 className="card-title text-center">{title}</h5>
+                            <p style={{color:"red"}}>
                               Price:{price}
                             </p>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-dark">Buy now</a>
+                            <div className='text-center'>
+                              <a href="#" className="btn btn-dark ">Buy now</a>
+                            </div>
+                            
                           </div>
 
                         </div>
